97b9ae84a53dc1586d1ddb727197234d59eb76fb
luisaraujoc <luislk_@outlook.com>
2023-05-11 13:03:35 -0300

    adicionando fontes e reestruturando pastas

---
+++ lib/app.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      child: Center(
+        child: Text('Home'),
+      ),
+    ));
+  }
+}
