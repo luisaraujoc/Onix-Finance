0f7ef77f00d0b8e7622b3afac6ad5781a9c1a6a6
luisaraujoc <luislk_@outlook.com>
2023-05-12 08:13:13 -0300

    Criando um onboarding

--- lib/freatures/onboarding/onboarding.dart
+++ lib/freatures/onboarding/onboarding.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -13,10 +15,105 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue,
+              Colors.indigo,
+            ],
+          ),
         ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(),
+            ),
+            Expanded(
+              child: Container(
+                child: BottomZoneGetStarted(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BottomZoneGetStarted extends StatelessWidget {
+  const BottomZoneGetStarted({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.2),
+        ),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 26.0),
+            child: Center(
+              child: Text(
+                "Finance Rakkii",
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w600,
+                  fontSize: 46,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Ajudando você a se controlar\nsua carteira",
+              style: TextStyle(
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Container(
+              height: 56,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.blue,
+                    Colors.indigo,
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(30),
+                ),
+              ),
+              child: Center(
+                child: Text(
+                  "Get Start",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
